b12e25398900388808bfe94a826490e5c6df6359
yms13691263347 <yangmiaoshan19@.163.com>
2019-06-23 21:21:34 +0800

    用户登录-发送登录请求完成基本登录流程

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -15,12 +15,11 @@
             <el-input v-model="form.code" placeholder="验证码"></el-input>
           </el-col>
           <el-col :offset="1" :span="9">
-            <!-- <el-button>获取验证码</el-button> -->
             <el-button @click="handleSendCode">获取验证码</el-button>
           </el-col>
         </el-form-item>
         <el-form-item>
-          <el-button class="btn-login" type="primary" @click="onSubmit">登录</el-button>
+          <el-button class="btn-login" type="primary" @click="handleLogin">登录</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -41,8 +40,25 @@ export default {
     }
   },
   methods: {
-    onSubmit() {
-      console.log('submit!')
+    handleLogin() {
+      axios({
+        method: 'POST',
+        url: 'http://ttapi.research.itcast.cn/mp/v1_0/authorizations',
+        data: this.form
+      })
+        .then(res => { // >=200 && < 400 的状态码会进入 then 成功
+          console.log(res.data)
+          this.$message({
+            message: '登录成功',
+            type: 'success'
+          })
+          this.$router.push({
+            name: 'home'
+          })
+        })
+        .catch((e) => {
+          this.$message.error('登录失败，手机号或验证码错误')
+        }) // >= 400 的状态码都会进入这里
     },
 
     handleSendCode() {
