335aa612ed75eef573c8ddc8a7a4d484cec3dce0
yms13691263347 <yangmiaoshan19@.163.com>
2019-06-25 20:28:36 +0800

    async-await && 将axios扩展到vue原型中

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -45,9 +45,9 @@
   </div>
 </template>
 <script>
-import axios from 'axios'
 import '@/vendor/gt' // 引入极验 JavaScript SDK 文件，通过 window.initGeetest 使用
 import { saveUser } from '@/utils/auth' // 按需加载，加载模块中非 export default 成员
+import initGeetest from '@/utils/init-geetest'
 const initCodeTimeSeconds = 60
 
 export default {
@@ -90,27 +90,26 @@ export default {
         this.submitLogin()
       })
     },
-    submitLogin() {
-      axios({
-        method: 'POST',
-        url: 'http://ttapi.research.itcast.cn/mp/v1_0/authorizations',
-        data: this.form
-      })
-        .then(res => { // >=200 && < 400 的状态码会进入 then 成功
-          const userInfo = res.data.data
-          // window.localStorage.setItem('user_info', JSON.stringify(userInfo))
-          saveUser(userInfo)
-          this.$message({
-            message: '登录成功',
-            type: 'success'
-          })
-          this.$router.push({
-            name: 'home'
-          })
+    async submitLogin() {
+      try {
+        const res = await this.$http({
+          method: 'POST',
+          url: '/authorizations',
+          data: this.form
         })
-        .catch((e) => {
-          this.$message.error('登录失败，手机号或验证码错误')
-        }) // >= 400 的状态码都会进入这里
+        const userInfo = res.data.data
+        // window.localStorage.setItem('user_info', JSON.stringify(userInfo))
+        saveUser(userInfo)
+        this.$message({
+          message: '登录成功',
+          type: 'success'
+        })
+        this.$router.push({
+          name: 'home'
+        })
+      } catch (err) {
+        this.$message.error('登录失败，手机号或验证码错误')
+      }
     },
     handleSendCode() {
       // 验证手机号是否有效
@@ -125,50 +124,44 @@ export default {
     /**
      * 验证通过，初始化显示人机交互验证码
      */
-    showGeetest() {
+    async showGeetest() {
       // 任何函数中的 function 函数内部的 this 指向 window
       const { mobile } = this.form
-      axios({
+      const res = await this.$http({
         method: 'GET',
-        url: `http://ttapi.research.itcast.cn/mp/v1_0/captchas/${mobile}`
-      }).then(res => {
-        const { data } = res.data
-        window.initGeetest({
-          // 以下配置参数来自服务端 SDK
-          gt: data.gt,
-          challenge: data.challenge,
-          offline: !data.success,
-          new_captcha: data.new_captcha,
-          product: 'bind' // 隐藏，直接弹出式
-        }, captchaObj => {
-          captchaObj.onReady(() => {
-            // 验证码ready之后才能调用verify方法显示验证码
-            captchaObj.verify() // 弹出验证码内容框
-          }).onSuccess(() => {
-            // your code
-            const {
-              geetest_challenge: challenge,
-              geetest_seccode: seccode,
-              geetest_validate: validate } =
+        url: `/captchas/${mobile}`
+      })
+      const { data } = res.data
+      const captchaObj = await initGeetest({
+        // 以下配置参数来自服务端 SDK
+        gt: data.gt,
+        challenge: data.challenge,
+        offline: !data.success,
+        new_captcha: data.new_captcha,
+        product: 'bind' // 隐藏，直接弹出式
+      })
+      captchaObj.onReady(() => {
+        // 验证码ready之后才能调用verify方法显示验证码
+        captchaObj.verify() // 弹出验证码内容框
+      }).onSuccess(async() => {
+        // your code
+        const {
+          geetest_challenge: challenge,
+          geetest_seccode: seccode,
+          geetest_validate: validate } =
             captchaObj.getValidate()
-            axios({
-              method: 'GET',
-              url: `http://ttapi.research.itcast.cn/mp/v1_0/sms/codes/${mobile}`,
-              params: {
-                challenge,
-                validate,
-                seccode
-              }
-            }).then(res => {
-              // 发送短信成功，开始倒计时
-              this.codeCountDown()
-            })
-          }).onError(function() {
-            // your code
-          })
-          // 在这里注册 “发送验证码” 按钮的点击事件，然后验证用户是否输入手机号以及手机号格式是否正确，没有问题：
-          // captchaObj.verify
+            // 发送短信
+        await this.$http({
+          method: 'GET',
+          url: `/sms/codes/${mobile}`,
+          params: {
+            challenge,
+            validate,
+            seccode
+          }
         })
+        // 发送短信成功，开始倒计时
+        this.codeCountDown()
       })
     },
     /**
