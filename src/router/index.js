cb2465bd6441a176d2588bd44c8eb183af28a6dd
yms13691263347 <yangmiaoshan19@.163.com>
2019-06-26 19:14:14 +0800

    文章列表-组件布局

--- src/router/index.js
+++ src/router/index.js
@@ -32,6 +32,11 @@ const router = new Router({
         name: 'publish',
         path: '/publish',
         component: () => import('@/views/publish')
+      },
+      {
+        name: 'article',
+        path: '/article',
+        component: () => import('@/views/article')
       }
       ]
     },
