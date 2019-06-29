3182845982e7ce3732be06c023107f2534126e52
yms13691263347 <yangmiaoshan19@.163.com>
2019-06-29 21:36:11 +0800

    账户设置-加载基本信息

--- src/router/index.js
+++ src/router/index.js
@@ -47,6 +47,11 @@ const router = new Router({
         name: 'comment',
         path: '/comment',
         component: () => import('@/views/comment')
+      },
+      {
+        name: 'account',
+        path: '/account',
+        component: () => import('@/views/account')
       }
       ]
     },
