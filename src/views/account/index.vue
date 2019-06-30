23a3a36dcf6537b89efd5c6db7bced9ebccc5b44
yms13691263347 <yangmiaoshan19@.163.com>
2019-06-30 16:19:16 +0800

    修改vuex中的数据状态

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -85,7 +85,7 @@ export default {
     async handleSave() {
       try {
         const { name, intro, email } = this.user
-        await this.$http({
+        const data = await this.$http({
           method: 'PATCH',
           url: '/user/profile',
           data: {
@@ -98,6 +98,10 @@ export default {
           type: 'success',
           message: '保存修改成功'
         })
+        // 提交 mutation，也就是调用 mutation 函数
+        this.$store.commit('changeUser', data)
+        // 不要直接这样修改，无法通过调试工具查看最新的容器数据状态，也观测不到修改 state 数据的历史记录
+        // this.$store.state.user.name = '哈哈哈'
       } catch (err) {
         console.log(err)
         this.$message.error('保存修改失败')
@@ -118,6 +122,7 @@ export default {
           data: formData
         })
         this.user.photo = data.photo
+        this.$store.commit('changeUser', this.user)
         this.$message({
           type: 'success',
           message: '修改媒体头像成功'
