c55381dcf89c8a720740973014e56a69fc68d349
yms13691263347 <yangmiaoshan19@.163.com>
2019-07-01 15:23:31 +0800

    echarts使用

---
+++ src/views/fans/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <!-- 画布容器 -->
+    <div id="main" style="width: 600px;height:400px;"></div>
+  </div>
+</template>
+<script>
+// 1. npmi echarts
+// 2. 在哪里用就在哪里 import 引入
+// 3. 在模板中准备一个具有宽高画布容器
+// 4. 在组件的 mounted 初始化图表
+import echarts from 'echarts'
+
+export default {
+  name: 'FansIndex',
+  data() {
+    return {}
+  },
+  async mounted() {
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById('main'))
+
+    // 指定图表的配置项和数据
+    var option = {
+      title: {
+        text: 'ECharts 入门示例'
+      },
+      tooltip: {},
+      legend: {
+        data: ['销量']
+      },
+      xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+      },
+      yAxis: {},
+      series: [{
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+      }]
+    }
+
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option)
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
